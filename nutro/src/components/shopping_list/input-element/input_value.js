5005b126fefbcb812df742c2968d42f28ab3c647
Yasminamayeh <yasminmayeh@gmail.com>
2021-07-03 13:48:09 +0100

    index.js, one folder

---
+++ nutro/src/components/shopping_list/input-element/input_value.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react'
+
+class componentName extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    componentWillMount () {
+
+    }
+
+    componentDidMount () {
+
+    }
+
+    componentWillReceiveProps (nextProps) {
+
+    }
+
+    shouldComponentUpdate (nextProps, nextState) {
+
+    }
+
+    componentWillUpdate (nextProps, nextState) {
+
+    }
+
+    componentDidUpdate (prevProps, prevState) {
+
+    }
+
+    componentWillUnmount () {
+
+    }
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+componentName.propTypes = {
+
+}
+
+export default componentName
