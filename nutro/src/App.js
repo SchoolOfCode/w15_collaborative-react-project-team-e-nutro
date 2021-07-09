ff5c3ebf1e9f7df99aa9be356e78c5ce1e99d312
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-07-09 15:11:37 +0100

    Imported Nunito Sans font

--- nutro/src/App.js
+++ nutro/src/App.js
@@ -1,10 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import BackgroundLandingPage from "./components/background_landing-page"
+import BackgroundLandingPage from "./components/background_landing-page";
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <BackgroundLandingPage />
         <a
           className="App-link"
           href="https://reactjs.org"
