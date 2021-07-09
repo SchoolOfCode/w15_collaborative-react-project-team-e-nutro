670230173d52d7dbaa99c1208523c62b5744d10b
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-07-09 15:41:47 +0100

    Added background images

--- nutro/src/components/background_landing-page/index.js
+++ nutro/src/components/background_landing-page/index.js
@@ -2,15 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import "./styles.css";
+import "./green-ellipse.png";
+import "./red-ellipse.png";
+import "./yellow-ellipse.png";
 
 function BackgroundLandingPage() {
-    return (
-        <div className="Background-landing-page">
-        <h1 className="title">Nutro</h1>
-        <h3 className="sub-text">Help the world we live in by helping yourself  first.</h3>
-        </div>
-        
-    );
+  return (
+    <div className="Background-landing-page">
+      <h1 className="title">Nutro</h1>
+      <h3 className="sub-text">
+        Help the world we live in by helping yourself first.
+      </h3>
+      <div className="green-ellipse"></div>
+      <div className="yellow-ellipse"></div>
+      <div className="red-ellipse"></div>
+    </div>
+  );
 }
 
 export default BackgroundLandingPage;
