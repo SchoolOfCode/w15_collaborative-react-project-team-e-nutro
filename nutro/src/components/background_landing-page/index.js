bfbb7bc29327b4212e0b318ab71591f5609fb3ca
Yasminamayeh <yasminmayeh@gmail.com>
2021-07-10 09:18:08 +0100

    final styling

--- nutro/src/components/background_landing-page/index.js
+++ nutro/src/components/background_landing-page/index.js
@@ -9,13 +9,13 @@ import "./yellow-ellipse.png";
 function BackgroundLandingPage() {
   return (
     <div className="Background-landing-page">
+      <div className="green-ellipse"></div>
+      <div className="yellow-ellipse"></div>
+      <div className="red-ellipse"></div>
       <h1 className="title">Nutro</h1>
       <h3 className="sub-text">
         Help the world we live in by helping yourself first.
       </h3>
-      <div className="green-ellipse"></div>
-      <div className="yellow-ellipse"></div>
-      <div className="red-ellipse"></div>
     </div>
   );
 }
