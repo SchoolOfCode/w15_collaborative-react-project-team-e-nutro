7ac162450aaf22f242091308a5db93ad3db71a5f
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-07-10 11:18:19 +0100

    Added goals

--- nutro/src/components/input_signup/index.js
+++ nutro/src/components/input_signup/index.js
@@ -14,6 +14,10 @@ function Input() {
     setDiet(e.target.value);
   }
 
+  function handleGoalsChange(e) {
+    setGoals(e.target.value);
+  }
+
   return (
     (
       <input
@@ -67,6 +71,25 @@ function Input() {
         <option value="Vegetarian">Vegetarian</option>
         <option value="Pescetarian">Pescetarian</option>
         <option value="Gluten Free">Gluten Free</option>
+      </select>
+    )),
+    ((<label for="goals">Select your goal:</label>),
+    (
+      <select
+        name="goal"
+        id="goal"
+        defaultValue={goals}
+        onChange={handleGoalsChange}
+      >
+        <option value="Lose weight">Lose weight</option>
+        <option value="Help the environment">Help the environment</option>
+        <option value="Become helthier and feel better">
+          Become healthier and feel better
+        </option>
+        <option value="Lower carbon footprint">Lower carbon footprint</option>
+        <option value="Shop ethically/sustainably">
+          Shop ethically/sustainably
+        </option>
       </select>
     ))
   );
